1621e2691e2ba6b43b18bce49b34df53d2968bef
SajjanKarn <49333264+SajjanKarn@users.noreply.github.com>
2020-11-20 13:42:40 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const lyrics = require("simple-get-lyrics");
+
+const app = express();
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(require("cors")());
+
+// routes
+app.post("/get/lyrics", (req, res) => {
+  const { artistName, songName } = req.body;
+
+  if (!artistName || !songName) {
+    return res.json({ error: "Please enter both fields!😒" });
+  }
+
+  // find the lyrics
+  lyrics
+    .search(artistName, songName)
+    .then((response) => {
+      return res.json(response);
+    })
+    .catch((err) => {
+      if (err) {
+        return res.json({ error: "No match found!😥" });
+      }
+    });
+});
+
+// server config
+app.listen(3000, () => {
+  console.log("server started listening on port: 3000....");
+});
